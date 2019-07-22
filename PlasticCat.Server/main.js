e90d261062e3f7fa286a1928b63ef02e55739ba3
Imants Volkovs <imants.volkovs@gmail.com>
2019-07-22 23:08:14 +0300

    first repo initial

--- PlasticCat.Server/main.js
+++ PlasticCat.Server/main.js
@@ -2,24 +2,36 @@
 var express = require('express');
 var path = require('path');
 var open = require('open');
+var clientsRepo = require('./repos/clients.repo');
 
 var port = 3000;
 var APP_FOLDER = '../wwwroot';
 var app = express();
 
 // Static files
-app.get('*.*', express.static(APP_FOLDER, {maxAge: '1y'}));
+app.get('*.*', express.static(APP_FOLDER, { maxAge: '1y' }));
 
 // ---- SERVE APLICATION PATHS
 app.all('*', function (req, res) {
-    res.status(200).sendFile(`/`, {root: APP_FOLDER});
+    res.status(200).sendFile(`/`, { root: APP_FOLDER });
 });
 
 app.listen(port, function listenFn(error) {
-    if(error !== undefined){
+    if (error !== undefined) {
         console.log(error);
     }
     else {
-        open('http://localhost:'+ port);
+        const path = `http://localhost:${port}`;
+        open(path);
+        console.log(`Started Server at ${path}`);
     }
+});
+
+const API_BASE_ROUTE = '/api'
+// TODO add routes
+clientsRepo(app);
+
+app.get('/settings', function (req, res) {
+    const settings = {};
+    res.send(settings)
 })
---
+++ PlasticCat.Server/route.constants.js
@@ -0,0 +1,3 @@
+export const ROUTE_CONSTANTS = {
+    BASE_URL = '/api'
+};
